d15c52689daeda9b904291b16b3e1f9cab599f01
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-06-11 17:31:14 -0500

    Agregando instrucciones completas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-helpButton = getElementById('help-button');
+const helpButton = document.getElementById('help-button');
+const closeButton = document.getElementById('close');
 
-const hideSection = () => {
+const hideSection = (id) => {
   document.getElementById(id).classList.add('hide');
 }
 
-const showSection = () => {
+const showSection = (id) => {
   document.getElementById(id).classList.remove('hide');
 }
 
-const showInstructions = () => {
-  hideSection('first-page');
-  showSection('instructions');
+const showInstructions = () => { showSection('instructions');}
 
-}
+const closeInstructions = () => {hideSection('instructions');}
 
-helpButton.addEventListener("click", showInstructions());
+helpButton.addEventListener("click", showInstructions);
+closeButton.addEventListener("click", closeInstructions)
